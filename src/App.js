2dddb74bc35218868d6e2df779d0b49cbea1218b
aaronkh <huang.aaron03@gmail.com>
2022-07-22 02:19:00 -0700

    revamp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./screens/Landing";
 import Test from "./screens/Test";
 import styled from "styled-components";
